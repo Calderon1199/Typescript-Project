ec0cfe939912b403dd9272249396d0e95f740af7
Daniel Calderon <danielcalderon1199@gmail.com>
2024-07-19 21:01:13 -0700

    Adding custom middleware to send errors code to the frontend within middlewares/index.js and a user validation chain file

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../app/Controllers/UserController');
+const validateUser = require('../app/Middlewares/ValidateUsers');
+const validate = require('../app/Middlewares');
 
 /* GET users listing. */
-router.post('/', UserController.userStore);
+router.post('/', validateUser(), validate, UserController.userStore);
 router.get('/lists', UserController.usersLists);
 router.put('/:id', UserController.updateUser);
 router.delete('/:id', UserController.deleteUser);
